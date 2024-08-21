f2a1808d75db1f7fcd5edf2884dbefb8b8c20983
Pedro <pedrorosemberg@aol.com>
2024-08-21 13:07:53 -0300

    [Update] Clients

--- src/components/Clientes.js
+++ src/components/Clientes.js
@@ -4,6 +4,9 @@ const Clientes = () => {
   return (
     <section id="clientes" className="py-10 px-5 bg-gray-800">
       <h2 className="text-3xl text-teal-400">Clientes de Programação</h2>
+      <p className="text-lg mb-20">Galeria de clientes.</p>
+
+      <h2 className="text-3xl text-teal-400">Clientes de Marketing</h2>
       <p className="text-lg">Galeria de clientes.</p>
     </section>
   );
