9e3092f954920a68f79763c073a4a58b178a20e1
Pedro <pedrorosemberg@aol.com>
2024-08-21 02:08:14 -0300

    Update Tecnologia Compontents

--- src/components/Tecnologias.js
+++ src/components/Tecnologias.js
@@ -4,38 +4,51 @@ import { faHtml5, faCss3Alt, faJs, faPython } from '@fortawesome/free-brands-svg
 import { faNodeJs } from '@fortawesome/free-brands-svg-icons';
 import { faReact } from '@fortawesome/free-brands-svg-icons';
 import { faTailwindcss } from '@fortawesome/free-brands-svg-icons';
+import { faBootstrap } from '@fortawesome/free-brands-svg-icons';
 
 const Tecnologias = () => {
   const [technologies, setTechnologies] = useState([]);
+
   return (
     <section id="tecnologias" className="py-10 px-5 bg-gray-800">
-      <h2 className="text-3xl text-teal-400">Tecnologias Utilizadas</h2>
-      <ul className="mt-5">
-        <li className="flex items-center text-lg text-white">
-          <FontAwesomeIcon icon={faHtml5} className="w-8 h-8 mr-2" /> HTML5
-        </li>
-        <li className="flex items-center text-lg text-white">
-          <FontAwesomeIcon icon={faCss3Alt} className="w-8 h-8 mr-2" /> CSS3
-        </li>
-        <li className="flex items-center text-lg text-white">
-          <FontAwesomeIcon icon={faJs} className="w-8 h-8 mr-2" /> JavaScript
-        </li>
-        <li className="flex items-center text-lg text-white">
-          <FontAwesomeIcon icon={faPython} className="w-8 h-8 mr-2" /> Python
-        </li>
-      </ul>
-      <h3 className="text-2xl text-teal-400 mt-5">Bibliotecas e Frameworks</h3>
-      <ul className="mt-5">
-        <li className="flex items-center text-lg text-white">
-          <FontAwesomeIcon icon={faReact} className="w-8 h-8 mr-2" /> React.js
-        </li>
-        <li className="flex items-center text-lg text-white">
-          <FontAwesomeIcon icon={faNodeJs} className="w-8 h-8 mr-2" /> Node.js
-        </li>
-        <li className="flex items-center text-lg text-white">
-          <FontAwesomeIcon className="w-8 h-8 mr-2" /> TailwindCSS
-        </li>
-      </ul>
+      <h2 className="text-3xl text-teal-400 text-center">Linguagens e Tecnologias utilizadas</h2>
+      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
+          <FontAwesomeIcon icon={faHtml5} className="w-8 h-8 mb-2" style={{ color: '#61dafb' }} />
+          <span className="text-lg text-white">HTML5</span>
+        </div>
+        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
+          <FontAwesomeIcon icon={faCss3Alt} className="w-8 h-8 mb-2" style={{ color: '#61dafb' }} />
+          <span className="text-lg text-white">CSS3</span>
+        </div>
+        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
+          <FontAwesomeIcon icon={faJs} className="w-8 h-8 mb-2" style={{ color: '#61dafb' }} />
+          <span className="text-lg text-white">JavaScript</span>
+        </div>
+        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
+          <FontAwesomeIcon icon={faPython} className="w-8 h-8 mb-2" style={{ color: '#61dafb' }} />
+          <span className="text-lg text-white">Python</span>
+        </div>
+      </div>
+      <h3 className="text-2xl text-teal-400 mt-5 text-center">Bibliotecas e Frameworks</h3>
+      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
+          <FontAwesomeIcon icon={faReact} className="w-8 h-8 mb-2" style={{ color: '#61dafb' }} />
+          <span className="text-lg text-white">React.js</span>
+        </div>
+        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
+          <FontAwesomeIcon icon={faNodeJs} className="w-8 h-8 mb-2" style={{ color: '#61dafb' }} />
+          <span className="text-lg text-white">Node.js</span>
+        </div>
+        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
+          <FontAwesomeIcon icon={faCss3Alt} className="w-8 h-8 mb-2" style={{ color: '#61dafb' }} />
+          <span className="text-lg text-white">TailwindCSS</span>
+        </div>
+        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
+          <FontAwesomeIcon icon={faBootstrap} className="w-8 h-8 mb-2" style={{ color: '#61dafb' }} />
+          <span className="text-lg text-white">Bootstrap</span>
+        </div>
+      </div>
     </section>
   );
 };
