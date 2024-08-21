9ddf5668918150dae7ac1f31dcffe0f89839cf1c
Pedro <pedrorosemberg@aol.com>
2024-08-21 17:57:33 -0300

    [Update] Sobre.js

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -4,7 +4,11 @@ const Sobre = () => {
   return (
     <section id="sobre" className="py-10 px-5 bg-gray-800">
       <h2 className="text-3xl text-teal-400">Sobre</h2>
-      <p className="text-lg">Um resumo das minhas formações e experiências.</p>
+      <p className="text-lg">Sou graduado em Marketing, possuo MBA em Gestão Comerccial e Inteligência de Mercado.
+      <p>
+        Pós-graduando em Psicanálise Clínica
+      </p>
+      </p>
     </section>
   );
 };
