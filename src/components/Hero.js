4729ff7a3ffed79ad30bddf26f2a7860dcd43fb4
Pedro <pedrorosemberg@aol.com>
2024-08-21 22:05:09 -0300

    Update Hero.js
    Includes an fuck main writer.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 
 const Hero = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      strings: ['Mercadólogo. Desenvolvedor. Pesquisador.', 'Criatividade. Inovação. Tecnologia.'],
+      typeSpeed: 60,
+      backSpeed: 60,
+      loop: true,
+    };
+
+    const typed = new Typed(el.current, options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <main className="hero h-screen flex items-center justify-center bg-gray-900">
       <section className="intro flex justify-between items-center px-10">
         <div className="content max-w-lg">
-          <h1 className="text-5xl mb-5 style={{ color: '#61dafb' }}">Olá!<br />Bem-vindo ao meu portfólio.</h1>
-          <p className="text-lg mb-8 text-white">Desenvolvedor Full Stack e Futuro Engenheiro de Software.</p>
+          <h1 className="text-5xl mb-5" style={{ color: '#61dafb' }}>
+            Olá!
+          </h1>
+          <h2 className="text-3xl mb-5 text-teal-400">
+            Eu sou o <span style={{ color: '#61dafb' }}>Pedro Rosemberg</span>,
+            
+          </h2>
+          <p className="text-lg mb-8 text-white">
+            <span ref={el} />
+          </p>
         </div>
       </section>
     </main>
