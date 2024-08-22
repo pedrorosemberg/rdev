e3c02d8f35ea65f84b6b879e1801383bb3c20685
Pedro <pedrorosemberg@aol.com>
2024-08-21 22:28:56 -0300

    Update Partners's reponsivity

--- src/components/Parceiros.js
+++ src/components/Parceiros.js
@@ -12,18 +12,18 @@ import parceiroWix from '../assets/parceiros/wix.png';
 const Parceiros = () => {
   return (
     <section id="parceiros" className="py-10 px-5 bg-gray-800">
-      <h2 className="text-3xl text-teal-400">Parceiros</h2>
+      <h2 className="text-3xl text-teal-400 text-center">Parceiros</h2>
       <div className="flex flex-wrap justify-center mt-5">
-        <div className="flex justify-center">
-          <img src={parceiroBiud} alt="Biud Tecnologia" className="m-4 h-8 w-auto" />
-          <img src={parceiroCora} alt="Cora SCD" className="m-4 h-6 w-auto" />
-          <img src={parceiroGoogle} alt="Google" className="m-4 h-8 w-auto" />
-          <img src={parceiroMeta} alt="Meta" className="m-4 h-8 w-auto" />
-          <img src={parceiroMicrosoft} alt="Microsoft" className="m-4 h-8 w-auto" />
-          <img src={parceiroNotion} alt="Notion" className="m-4 h-8 w-auto" />
-          <img src={parceiroNuvemshop} alt="Nuvemshop" className="m-4 h-8 w-auto" />
-          <img src={parceiroUol} alt="uol" className="m-4 h-8 w-auto" />
-          <img src={parceiroWix} alt="Wix" className="m-4 h-6 w-auto" />
+        <div className="flex flex-wrap justify-center">
+          <img src={parceiroBiud} alt="Biud Tecnologia" className="m-2 h-10 w-auto max-w-xs" />
+          <img src={parceiroCora} alt="Cora SCD" className="m-2 h-10 w-auto max-w-xs" />
+          <img src={parceiroGoogle} alt="Google" className="m-2 h-10 w-auto max-w-xs" />
+          <img src={parceiroMeta} alt="Meta" className="m-2 h-10 w-auto max-w-xs" />
+          <img src={parceiroMicrosoft} alt="Microsoft" className="m-2 h-10 w-auto max-w-xs" />
+          <img src={parceiroNotion} alt="Notion" className="m-2 h-10 w-auto max-w-xs" />
+          <img src={parceiroNuvemshop} alt="Nuvemshop" className="m-2 h-10 w-auto max-w-xs" />
+          <img src={parceiroUol} alt="UOL" className="m-2 h-10 w-auto max-w-xs" />
+          <img src={parceiroWix} alt="Wix" className="m-2 h-10 w-auto max-w-xs" />
         </div>
       </div>
     </section>
