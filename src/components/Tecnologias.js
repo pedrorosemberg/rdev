742dda65e6c9afb4ec89a96d127bed7445acba9d
Pedro <pedrorosemberg@aol.com>
2024-08-21 22:04:32 -0300

    Update Tecnologia.js
    Optimize the blocks

--- src/components/Tecnologias.js
+++ src/components/Tecnologias.js
@@ -4,7 +4,7 @@ import { faHtml5, faCss3Alt, faJs, faPython } from '@fortawesome/free-brands-svg
 import { faNodeJs } from '@fortawesome/free-brands-svg-icons';
 import { faReact } from '@fortawesome/free-brands-svg-icons';
 import { faBootstrap } from '@fortawesome/free-brands-svg-icons';
-import { DatabaseIcon, CodeIcon, SparklesIcon, BeakerIcon, LightningBoltIcon, AdjustmentsIcon, TagIcon, FolderAddIcon, FolderDownloadIcon, FolderOpenIcon, FolderIcon, FolderRemoveIcon, ClipboardIcon, ClipboardCheckIcon, PaperClipIcon, DesktopComputerIcon, SwitchVerticalIcon, DocumentSearchIcon } from '@heroicons/react/solid';
+import { DatabaseIcon, BeakerIcon, LightningBoltIcon, AdjustmentsIcon, TagIcon, DesktopComputerIcon, FolderOpenIcon, SwitchVerticalIcon, DocumentSearchIcon } from '@heroicons/react/solid';
 
 
 const Tecnologias = () => {
@@ -14,7 +14,7 @@ const Tecnologias = () => {
     <section id="tecnologias" className="py-10 px-5 bg-gray-800">
       <h2 className="text-3xl text-teal-400 text-center mb-6">Stacks e Habilidades</h2>
       <h3 className="text-2xl text-teal-400 text-center">Linguagens e Tecnologias</h3>
-      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="mt-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
           <FontAwesomeIcon icon={faHtml5} className="w-8 h-8 mb-2 text-teal-400" />
           <span className="text-lg text-white">HTML5</span>
@@ -44,35 +44,35 @@ const Tecnologias = () => {
           <span className="text-lg text-white">NoSQL</span>
         </div>
         <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
-          <FolderRemoveIcon className="w-8 h-8 mb-2 text-teal-400" />
+          <FolderOpenIcon className="w-8 h-8 mb-2 text-teal-400" />
           <span className="text-lg text-white">Git</span>
         </div>
       </div>
       <h3 className="text-2xl text-teal-400 mt-5 text-center">Bibliotecas e Frameworks</h3>
-      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="mt-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
           <FontAwesomeIcon icon={faReact} className="w-8 h-8 mb-2 text-teal-400" />
           <span className="text-lg text-white">React.js</span>
-        </div>
-        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
-          <ClipboardCheckIcon className="w-8 h-8 mb-2 text-teal-400" />
-          <span className="text-lg text-white">TailwindCSS</span>
-        </div>
-        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
-          <FontAwesomeIcon icon={faBootstrap} className="w-8 h-8 mb-2" style={{ color: '#61dafb' }} />
-          <span className="text-lg text-white">Bootstrap</span>
         </div>
         <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
           <BeakerIcon className="w-8 h-8 mb-2 text-teal-400" />
           <span className="text-lg text-white">Jest</span>
         </div>
         <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
+          <FontAwesomeIcon icon={faBootstrap} className="w-8 h-8 mb-2" style={{ color: '#61dafb' }} />
+          <span className="text-lg text-white">Bootstrap</span>
+        </div>
+        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
           <TagIcon className="w-8 h-8 mb-2 text-teal-400" />
           <span className="text-lg text-white">Selenium</span>
         </div>
+        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
+          <AdjustmentsIcon className="w-8 h-8 mb-2 text-teal-400" />
+          <span className="text-lg text-white">TailwindCSS</span>
+        </div>
       </div>
       <h3 className="text-2xl text-teal-400 mt-5 text-center">Websites, Softwares e Programas</h3>
-      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="mt-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
           <DesktopComputerIcon className="w-8 h-8 mb-2 text-teal-400" />
           <span className="text-lg text-white">VSCode</span>
@@ -88,14 +88,6 @@ const Tecnologias = () => {
         <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
           <LightningBoltIcon className="w-8 h-8 mb-2 text-teal-400" />
           <span className="text-lg text-white">Supabase</span>
-        </div>
-        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
-          <AdjustmentsIcon className="w-8 h-8 mb-2 text-teal-400" />
-          <span className="text-lg text-white">Firebase</span>
-        </div>
-        <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
-          <FolderIcon className="w-8 h-8 mb-2 text-teal-400" />
-          <span className="text-lg text-white">GitLabs</span>
         </div>
         <div className="flex flex-col items-center bg-gray-700 p-4 rounded-lg">
           <DocumentSearchIcon className="w-8 h-8 mb-2 text-teal-400" />
