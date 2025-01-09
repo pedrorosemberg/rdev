4ed253ade8206bf7b5de0cab50246a8704bc18f0
PedroRosemberg <dev@pedrorosemberg.com>
2025-01-08 23:05:41 -0300

    Added new contact info

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Contato = () => {
   return (
-    <section id="contato" className="py-10 px-5 bg-gray-800">
+    <section id="contato" className="py-10 px-5 bg-gray-800 text-white">
       <h2 className="text-3xl text-teal-400">Contato</h2>
-      <p className="text-lg">Email: <a className="text-teal-400" href="mailto:pedro@codever.com.br">pedro@codever.com.br</a></p>
+      <p className="text-lg">Email: <a className="text-teal-400" href="mailto:dev@pedrorosemberg.com">dev@pedrorosemberg.com</a></p>
       <p className="text-lg">WhatsApp: <a className="text-teal-400" href="https://wa.me/5511934738412">11 93473-8412</a></p>
     </section>
   );
