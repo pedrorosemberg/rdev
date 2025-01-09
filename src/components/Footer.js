01c2be1aae556ffeb547b0c84e67c93b9ed8f9b4
PedroRosemberg <dev@pedrorosemberg.com>
2025-01-08 23:14:39 -0300

    Fix year and info on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,16 @@ import marketguruLogo from '../assets/footer/marketguru.png';
 import rosembergLogo from '../assets/footer/rosemberg.dev.png';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear(); // Obtém o ano atual
+
   return (
     <footer className="py-5 bg-gray-900 flex justify-between items-center">
-      <p className="text-white ml-5">&copy;2024 PEDRO ROSEMBERG.    <i>Versão 1.2</i></p>
+      <p className="text-white ml-5">
+        &copy; 2018-{currentYear} PEDRO ROSEMBERG. <i>Versão 1.3</i>
+      </p>
       <div className="flex items-center mr-5">
-        <a href="https://www.codever.com.br" target="_blank" rel="noopener noreferrer">
-          <img src={codeverLogo} alt="Codever" className="h-8 mx-2" />
-        </a>
-        <a href="https://www.marketguru.com.br" target="_blank" rel="noopener noreferrer">
-          <img src={marketguruLogo} alt="Marketguru" className="h-8 mx-2" />
-        </a>
-        <a href="https://www.pedrorosemberg.com" target="_blank" rel="noopener noreferrer">
-          <img src={rosembergLogo} alt="Rosemberg.dev" className="h-8 mx-2" />
+        <a href="https://dev.pedrorosemberg.com" target="_blank" rel="noopener noreferrer">
+          <img src={rosembergLogo} alt="dev.pedrorosemberg.com" className="h-8 mx-2" />
         </a>
       </div>
     </footer>
