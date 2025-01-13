4239f82bd8930a568d77dbe86b2f7c1a3bb758cd
PedroRosemberg <dev@pedrorosemberg.com>
2025-01-13 01:15:33 -0300

    Upgrade delete courses

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <Tecnologias />
       <Formacao />
       <Experiencias />
-      <Cursos />
+      {/* <Cursos /> */}
       <ProjetosPessoais />
       <ProjetosProfissionais />
       <Clientes />
